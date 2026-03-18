c4ff26e73cfa1c7ceba1dfa6405b6c350fad6ff2
S-Husan <241907483+S-Husan@users.noreply.github.com>
2026-03-18 23:32:24 +0500

    classes and objects

---
+++ Mitderm26.py
@@ -0,0 +1,25 @@
+# Bismillahir Rohmanir Rohim
+
+# Classes and Objects
+# class human:
+#     legs = 2  #class propeerties
+#     hands = 2
+#     eyes = 2
+# ali= human()
+# print(ali.legs)
+
+
+# Class with __init__ (haydar / zafar example)
+
+# class hydar:
+#     def __init__ (self, money ,car ,house):
+#         self.money = money
+#         self.car = car
+#         self.house = house 
+# # Create TWO objects from same blueprint
+
+# zafar = hydar("100$", "BMW","Gagarin")
+# ml = hydar("100$", "MERS","s")
+
+# print(zafar.money)
+# print(ml.house)
--- new-topics.py
+++ new-topics.py
@@ -1172,6 +1172,6 @@ import re
 
 
 # ndim show the quantity of dimentions
-import numpy as np
-a = np.array(10)
-print(a.ndim)
+
+
+# Access array elements
