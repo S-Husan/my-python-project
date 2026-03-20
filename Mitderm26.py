9f765cd8d9b3d3fcfdebe546bb3f8acd9c28254e
S-Husan <241907483+S-Husan@users.noreply.github.com>
2026-03-20 22:05:52 +0500

    Fuctorial machine

--- Mitderm26.py
+++ Mitderm26.py
@@ -1,25 +1,20 @@
 # Bismillahir Rohmanir Rohim
 
-# Classes and Objects
-# class human:
-#     legs = 2  #class propeerties
-#     hands = 2
-#     eyes = 2
-# ali= human()
-# print(ali.legs)
+# Recursion
+# Less time
+# Recursion calls itself directly or inderectly until the base problem is solved.
 
+# def fuctorial(n):
+#     if n == 1:
+#         return 1
+#     else:
+#         return n * fuctorial(n-1)
+    
+    
+# n = int(input("Enter a non-negative integer: "))
+# if n <0:
+#     print("FActorial is not defined for negative number: ")
+# else:
+#     result = fuctorial(n)
+#     print("The factorial of " , n ,"is " , result )
 
-# Class with __init__ (haydar / zafar example)
-
-# class hydar:
-#     def __init__ (self, money ,car ,house):
-#         self.money = money
-#         self.car = car
-#         self.house = house 
-# # Create TWO objects from same blueprint
-
-# zafar = hydar("100$", "BMW","Gagarin")
-# ml = hydar("100$", "MERS","s")
-
-# print(zafar.money)
-# print(ml.house)
