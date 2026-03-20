8f188e8e217ce3620fbcf669a7eafef476d91933
S-Husan <241907483+S-Husan@users.noreply.github.com>
2026-03-20 22:40:30 +0500

    Classas and objects revision for exam

--- Mitderm26.py
+++ Mitderm26.py
@@ -9,7 +9,9 @@
 #         return 1
 #     else:
 #         return n * fuctorial(n-1)
-    
+    # ===========================================
+    # ===========================================
+    # ===========================================
     
 # n = int(input("Enter a non-negative integer: "))
 # if n <0:
@@ -17,4 +19,45 @@
 # else:
 #     result = fuctorial(n)
 #     print("The factorial of " , n ,"is " , result )
+
+# ===========================================
+# Classes /Objects 
+
+# class MyCalss: #  <== MyClass here is class name
+    # x = 5 #  <==== 'X' here is property 
+# print(MyCalss.x)
+
+# ===========================================
+
+# Creating an object 
+
+# class MyCalss: #  <== MyClass here is class name
+    # x = 1 #  <==== 'X' here is property 
+# p1 =MyCalss() 
+
+# print(p1.x)
+# ===========================================
+
+# __init__() function
+# class human:
+#     def __init__(self,name,age):
+#         self.name= name # <==== self tells Python: "Store this data on THIS specific object"
+#         self.age= age   # <==== self tells Python: "Store this data on THIS specific object"
+# Person_one = human("Ali","19")
+# Person_two = human("Behruz","16")
+# print("His name is ",Person_one.name," and he is ",Person_one.age ,"years old")
+# print("His name is ",Person_two.name," and he is ",Person_two.age ,"years old")
+
+# ===========================================
+
+# Opject methods
+# class Person:
+#     def __init__(self,name,age):
+#         self.name= name
+#         self.age= age
+#     def myperson(self):
+#         print("My name is " + self.name)
+# me = Person("Husan","17")
+# me.myperson()
+
 
